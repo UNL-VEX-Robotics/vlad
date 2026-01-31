8a95566afb73dd8371a0a872fb5417a452b33a63
Timothy Gibbons <tjgibbons10@gmail.com>
2026-01-30 18:53:36 -0600

    style: Tabs

--- db/migrations/20260130213620-create-teams.cjs
+++ db/migrations/20260130213620-create-teams.cjs
@@ -1,36 +1,36 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Teams', {
-      id: {
-        allowNull: false,
-        primaryKey: true,
-        type: Sequelize.UUID
-      },
-      name: {
-        type: Sequelize.STRING
-      },
-      parent_team: {
-        type: Sequelize.UUID,
-        references: {
-          model: 'Teams',
-          key: 'id'
-        },
-        onUpdate: 'CASCADE',
-        onDelete: 'SET NULL'
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Teams');
-  }
+	async up(queryInterface, Sequelize) {
+		await queryInterface.createTable('Teams', {
+			id: {
+				allowNull: false,
+				primaryKey: true,
+				type: Sequelize.UUID
+			},
+			name: {
+				type: Sequelize.STRING
+			},
+			parent_team: {
+				type: Sequelize.UUID,
+				references: {
+					model: 'Teams',
+					key: 'id'
+				},
+				onUpdate: 'CASCADE',
+				onDelete: 'SET NULL'
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			}
+		});
+	},
+	async down(queryInterface, Sequelize) {
+		await queryInterface.dropTable('Teams');
+	}
 };
--- db/models/user_tasks.js
+++ db/models/user_tasks.js
@@ -1,31 +1,31 @@
 import { Model, DataTypes } from 'sequelize';
 
 export default (sequelize) => {
-  class UserTasks extends Model {
-    static associate(models) {
-      // define association here
-    }
-  }
+	class UserTasks extends Model {
+		static associate(models) {
+			// define association here
+		}
+	}
 
-  UserTasks.init({
-    id: {
-      type: DataTypes.UUID,
-      primaryKey: true,
-      defaultValue: DataTypes.UUIDV4,
-    },
-    user_id: {
-      type: DataTypes.UUID,
-      allowNull: false,
-    },
-    task_id: {
-      type: DataTypes.UUID,
-      allowNull: false,
-    },
-  },{
-    sequelize,
-    modelName: 'UserTasks',
-    tableName: 'UserTasks',
-  });
+	UserTasks.init({
+		id: {
+			type: DataTypes.UUID,
+			primaryKey: true,
+			defaultValue: DataTypes.UUIDV4,
+		},
+		user_id: {
+			type: DataTypes.UUID,
+			allowNull: false,
+		},
+		task_id: {
+			type: DataTypes.UUID,
+			allowNull: false,
+		},
+	},{
+		sequelize,
+		modelName: 'UserTasks',
+		tableName: 'UserTasks',
+	});
 
-  return UserTasks;
+	return UserTasks;
 };
