2833c889baddfbf68e33c91ff2af7a5eb6acc202
Rbussom <ryanbussom@gmail.com>
2026-03-31 19:16:02 -0500

    refactor: ran prettier

--- backend/controllers/settings.controller.js
+++ backend/controllers/settings.controller.js
@@ -1,9 +1,8 @@
-import pool from "../db.js";
+//import pool from "../db.js";
 import { withLayout } from "../views/layout.js";
 import { settingsPage } from "../views/settings.view.js";
 
-export const renderSettings = async (req, res) =>{
+export const renderSettings = (req, res) => {
     const content = settingsPage();
     res.send(withLayout("Settings", content, req));
-
-}
+};
